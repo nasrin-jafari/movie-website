5e165fb3d1aab6220c5a19220e0f4d3d60a4d938
nasrin jafari <nasrin.jafari770@gmail.com>
2023-10-02 13:55:50 +0330

    design page details

--- src/pages/details/carousels/Similar.jsx
+++ src/pages/details/carousels/Similar.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import useFetch from "../../../hooks/useFetch";
-import {Carousel} from "../../../components"
+import { Carousel } from "../../../components";
 const Similar = ({ mediaType, id }) => {
   const { data, loading, error } = useFetch(`/${mediaType}/${id}/similar`);
-
   const title = mediaType === "tv" ? "Similar TV Shows" : "Similar Movies";
-
   return (
     <Carousel
       title={title}

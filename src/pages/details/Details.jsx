5f26941654f8545765df26e9604bb407d43e73b7
nasrin jafari <nasrin.jafari770@gmail.com>
2023-10-02 13:54:47 +0330

    design page 404

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,6 +1,12 @@
 import useFetch from "../../hooks/useFetch";
 import { useParams } from "react-router-dom";
-import {DetailsBanner , Cast , VideosSection, Similar , Recommendation} from "./"
+import {
+  DetailsBanner,
+  Cast,
+  VideosSection,
+  Similar,
+  Recommendation,
+} from "./";
 const Details = () => {
   const { mediaType, id } = useParams();
   const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
